47d59e2e9cc0cb6cf788ddf32557d64122db555a
Paulo Gomes <pjgomes85@hotmail.com>
2022-11-08 15:27:23 +0000

    add props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,17 @@
-function ExpenseItem() {
-  return <h2>Expense Item!</h2>
+import './ExpensesItem.css';
+
+function ExpenseItem(props) {
+
+
+  return (
+    <div className='expense-item'>
+      <div>{props.date.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>£{props.amount}</div>
+      </div>
+    </div>
+  );
 }
-
-
 
 export default ExpenseItem;
