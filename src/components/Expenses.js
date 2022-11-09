54ca272ca72d0760dd99806666e348ba961e990e
Paulo Gomes <pjgomes85@hotmail.com>
2022-11-09 08:31:18 +0000

    cleaning code

---
+++ src/components/Expenses.js
@@ -0,0 +1,35 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+import "./Expenses.css"
+
+
+function Expenses(params) {
+
+  return (
+  <Card className="expenses">
+    <ExpenseItem
+      title={params.expense[0].title}
+      amount={params.expense[0].amount}
+      date={params.expense[0].date}>
+      </ExpenseItem>
+
+      <ExpenseItem
+      title={params.expense[1].title}
+      amount={params.expense[1].amount}
+      date={params.expense[1].date}>
+      </ExpenseItem>
+      <ExpenseItem
+      title={params.expense[2].title}
+      amount={params.expense[2].amount}
+      date={params.expense[2].date}>
+      </ExpenseItem>
+      <ExpenseItem
+      title={params.expense[3].title}
+      amount={params.expense[3].amount}
+      date={params.expense[3].date}>
+      </ExpenseItem>
+  </Card>
+  )
+};
+
+export default Expenses;
