39a5d07231b289427f987fc50dd995c0bfba6e1c
Paulo Gomes <pjgomes85@hotmail.com>
2022-11-13 07:15:40 +0000

    cleaning code from expense form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,9 +3,17 @@ import './NewExpense.css'
 import ExpenseForm from "./ExpenseForm";
 
 function NewExpense () {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    }
+  }
+
+
   return (
   <div className="new-expense">
-    <ExpenseForm />
+    <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
   </div>
   )
 };
